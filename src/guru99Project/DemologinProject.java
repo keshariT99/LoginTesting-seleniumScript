9ad5e02adb047db2f5d792bb59dba94c8a86c4be
Keshari Thennakoon <110615349+keshariT99@users.noreply.github.com>
2024-11-04 13:28:21 +0530

    update Demo java file

---
+++ src/guru99Project/DemologinProject.java
@@ -0,0 +1,18 @@
+package loginScript;
+
+public class DemologinProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BooksLkLoginProject loginProject = new BooksLkLoginProject();
+		
+		loginProject.InvokeBrowser();
+		
+		//loginProject.Registration();
+		
+		//loginProject.Login("ABCperson", "12345ABCperson*");
+		
+		//loginProject.SearchBook();
+	}
+
+}
